52805d5649a088bb893fd18e3785ec9f7bcffd6b
mc-uasdc <58667084+mc-uasdc@users.noreply.github.com>
2019-12-08 14:40:55 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".whiteBox").click(function() {
+    $(".whiteBox").fadeOut();
+$(".movieCover").fadeIn();
+});
+
+$(".blackBox").click(function() {
+    $(".blackBox").fadeOut();
+$(".myersCloset").fadeIn();
+});
